e1de97bf5c47e697f0919525ab865f418e40177d
SAMK6 <sam.feldman74@gmail.com>
2024-05-06 02:07:02 -0400

    bitboard/fen translator.h file added

---
+++ MARK1/src/Translator.h
@@ -0,0 +1,12 @@
+#ifndef TRANSLATOR_H
+#define TRANSLATOR_H
+
+#include "BitBoard.h"
+
+// takes in a FEN string, creates amd returns the coresspoding bitboards
+BitBoard fenToBitBoard(const char*);
+
+// takes in a bitBoard and turns it into a FEN string, pass in a dynamically alocated char array of size n and the null terminated FEN string is placed there
+void bitBoardToFen(BitBoard, char*);
+
+#endif
