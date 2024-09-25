add53323eeb0454e0a2e37ff176c1284230e86d6
SAMK6 <sam.feldman74@gmail.com>
2024-09-25 00:09:07 -0400

    testing move generator

---
+++ test_data/move_generator_test_data.py
@@ -0,0 +1,22 @@
+import chess.pgn
+import sys
+
+
+if len(sys.argv) < 2:
+    print("No parameter was passed!")
+    exit
+else:
+    input_file = sys.argv[1]
+
+with open(input_file) as f:
+    for line in f:
+        # strip the bad stuff off the FENs
+        FEN = line.strip('\ufeff')
+        FEN = FEN.strip('\n')
+
+        # set the board state
+        board = chess.Board()
+        board.set_fen(FEN)
+
+        print(FEN)
+        print(board.legal_moves.count)
